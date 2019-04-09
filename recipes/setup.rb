458a7ebd1a81819f9356351b5a9b6f930b260eae
root <root@hamlet.kaputbrain.com>
2019-04-08 17:06:26 -0700

    initial commit

---
+++ recipes/setup.rb
@@ -0,0 +1,15 @@
+package 'tree' do
+  action :install
+end
+
+package 'ntp' do
+  action :install
+end
+
+package 'git' do
+  action :install
+end
+
+service 'ntpd' do
+  action [ :enable, :start ]
+end
